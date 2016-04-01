cfd97aacd0618009f0493c47279d823745faa51a
Tanner May <tannerjmay@gmail.com>
2016-04-01 09:55:55 -0600

    Sierpinski triangles

--- src/part2/SierpinskiTriangle.java
+++ src/part2/SierpinskiTriangle.java
@@ -1,6 +1,8 @@
 package part2;
 import java.awt.Color;
 import java.awt.geom.Point2D;
+import java.awt.geom.Point2D.Double;
+
 import draw.StdDraw;
 
 
@@ -11,8 +13,8 @@ public class SierpinskiTriangle {
 	private Color foreground = StdDraw.YELLOW;
 	
 	public SierpinskiTriangle() {
-		StdDraw.setXscale(0, 150); 
-		StdDraw.setYscale(75, 150); 
+		StdDraw.setXscale(0, 150);
+		StdDraw.setYscale(75, 150);
 		
 		top = new Point2D.Double(75, 150);
 		right = new Point2D.Double(150, 75);
@@ -22,7 +24,18 @@ public class SierpinskiTriangle {
 	}
    
 	public void sierpinski(Point2D.Double a, Point2D.Double b, Point2D.Double c, int n) {
-	   // TODO: implement
+		if(n <= 0) {
+			return;
+		}
+		Double left = new Point2D.Double(c.x + Math.abs(c.x - a.x) / 2, c.y + Math.abs(c.y - a.y) / 2);
+		Double bottom = new Point2D.Double(a.x, b.y);
+		Double right = new Point2D.Double(a.x + Math.abs(b.x - a.x) / 2, c.y + Math.abs(b.y - a.y) / 2);
+		drawTriangle(a, b, c, StdDraw.WHITE);
+		drawTriangle(left, right, bottom, StdDraw.BLACK);
+		sierpinski(right, b, bottom, n-1);
+		sierpinski(left, bottom, c, n-1);
+		sierpinski(a, right, left, n-1);
+		
     }
    
 	public void drawTriangle(Point2D.Double a, Point2D.Double b, Point2D.Double c, Color color) {
@@ -40,6 +53,6 @@ public class SierpinskiTriangle {
 	
 	public static void main(String[] args) { 
 		SierpinskiTriangle triangle = new SierpinskiTriangle();
-		triangle.draw(3);
+		triangle.draw(16);
 	}
 }
